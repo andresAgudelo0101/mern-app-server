133ae788807f12e06470f0119ea0aeddbfc21ea3
andres <echeverry11298@gmail.com>
2022-06-08 10:54:54 -0500

    dejamos las bases

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app=express();
+
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+
+const CONNECTION_URL='mongodb+srv://andresz21:andres11298@cluster0.sguztwm.mongodb.net/?retryWrites=true&w=majority'
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(()=>app.listen(PORT,()=>console.log('server runng')))
+    .catch((error)=>console.log(error.messge));
+
